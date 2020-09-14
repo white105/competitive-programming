c8e4b823cc9cee8738e89f676b618feedcd8fd04
white105 <nickwhite1423@gmail.com>
2020-09-13 19:46:43 -0700

    initial commit

---
+++ Codeforces/4A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iostream>;
+
+using namespace std;
+
+int main() {
+   int w;
+   cin >> w;
+
+   if (w > 2 && w % 2 == 0) {
+       cout << "YES";
+   } else if (w == 2) {
+       cout << "NO";
+   } else {
+       cout << "NO";
+   }
+}
